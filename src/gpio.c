f0d9ef5bc210af702c389ae2e957ab88504b07ce
fernan2crdz <fernan2crdz3550@gmail.com>
2025-10-02 15:00:19 -0300

    documentacion

--- src/gpio.c
+++ src/gpio.c
@@ -63,7 +63,7 @@ void MX_GPIO_Init(void)
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
   /*Configure GPIO pin : PA9 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9;
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
